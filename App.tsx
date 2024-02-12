5378e3d8596954254cce1b5d22ed942584197174
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-12 02:21:33 -0300

    Iniciado pagina inicial

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Bat Sinal!</Text>
-      <StatusBar style="auto" />
+      <Home></Home>
+      <StatusBar style="dark" />
     </View>
   );
 }
