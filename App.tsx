e6dd76889c0a51ff9c5daf861718f3eb701fc24d
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-12 03:59:05 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/screens/Home/Home";
+import Form from "./src/screens/Form/Form";
 
 export default function App() {
+  const [openForm, setOpenForm] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Home></Home>
+      {openForm === true ? <Form></Form> : <Home></Home>}
       <StatusBar style="dark" />
     </View>
   );
